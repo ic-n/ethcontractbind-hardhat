f4e05f77d0bfad77e03d68fb4789a4feb6839325
Nikola Kiselev <ic.nikolai.kiselev@gmail.com>
2024-07-07 11:47:51 +0200

    feat: start over

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,8 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
-
-// This adds support for typescript paths mappings
-import 'tsconfig-paths/register';
-
-import '@typechain/hardhat';
-import '@nomicfoundation/hardhat-ethers';
-import '@nomicfoundation/hardhat-chai-matchers';
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: '0.8.24',
-  },
-  paths: {
-    sources: './src/services/blockchain/contracts',
-    artifacts: './src/services/blockchain/artifacts/artifacts',
-  },
+  solidity: "0.8.24",
 };
 
 export default config;
